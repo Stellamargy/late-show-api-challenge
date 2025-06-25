955d92c6ffc9400e1e3f0ef303516bfab8f28392
stlmargy <stellahmargy0211@gmail.com>
2025-06-25 15:31:27 +0300

    Binds app to db object and migrate object to app and db

--- server/app.py
+++ server/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from .config import Config
-from flask_sqlalchemy import SQLAlchemy
+from models import Appearance,Guest,User,Episode,db
+from flask_migrate import Migrate
+
 #Create a Flask app instance
 app=Flask(__name__)
 app.config.from_object(Config)
+#Binds app to db Object
+db.init_app(app)
+# Create and bind the migrate object to app and db
+migrate = Migrate(app, db)
+
 
