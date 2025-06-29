9a53a45ac874591b3fbe637c59faa927fd9fcb08
stlmargy <stellahmargy0211@gmail.com>
2025-06-29 13:31:14 +0300

    Creates extension module to prevent circular import

--- server/app.py
+++ server/app.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from .config import Config
-from server.models import Appearance,Guest,User,Episode,db
-from flask_migrate import Migrate
+from server.models import Appearance,Guest,User,Episode
 from flask_bcrypt import Bcrypt
 from server.controllers import user_bp,auth_bp
-from .extensions import bcrypt
+from .extensions import bcrypt,migrate,db
 
 #Create a Flask app instance
 app=Flask(__name__)
 app.config.from_object(Config)
 #Binds app to db Object
 db.init_app(app)
-# Create and bind the migrate object to app and db
-migrate = Migrate(app, db)
+#  bind the migrate object to app and db
+migrate.init_app(db=db,app=app)
 #Bind app to bcrypt
 bcrypt.init_app(app)
 
--- server/extensions.py
+++ server/extensions.py
@@ -1,3 +1,6 @@
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 bcrypt=Bcrypt()
+migrate =Migrate()
+db =SQLAlchemy()    
